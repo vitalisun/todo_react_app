edcca9ead1c106d6aa9b9d94cb7a4042f57f7c1a
Vitaly Udenkov <vitalisun@gmail.com>
2019-12-20 19:39:14 +0300

    Начал добавлять css

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TodoListItem from '../todo-list-item/todo-list-item';
-
+import './todo-list.css';
 
 const TodoList = ({todos})=>{
 
@@ -9,14 +9,14 @@ const elements = todos.map((item)=>{
     const { id, ...itemProps } = item;
 
     return(
-        <li key={id}>
+        <li key={id} className="list-group-item">
             <TodoListItem {...itemProps}/>
         </li>
     );
 });
 
     return (
-        <ul>
+        <ul className="list-group todo-list">
             {elements}
         </ul>
     );
