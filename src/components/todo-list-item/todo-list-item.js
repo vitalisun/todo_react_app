9e2c3fc75a5d9393a85826de051818c48152d37d
Vitaly Udenkov <vitalisun@gmail.com>
2020-01-13 09:43:16 +0300

    Добавлены оставшиеся обработчики, реализованы поиск и фильтрация.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,42 +5,31 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component{
 
-    state = {
-        done: false,
-        important: false
-    }
-
-    onLabelClick = () => {
-        this.setState({
-            done: true
-        });
-    }
-
-    onMarkImportant = () => {
-        this.setState({
-            important: true
-        });
-    }
-
     render(){
 
-        const { label} = this.props;
-        const { done, important } = this.state;
+        const {
+            label,
+            onDeleted,
+            onToggleImportant,
+            onToggleDone,
+            important,
+            done
+        } = this.props;
 
         let classNames = 'todo-list-item';
 
-        if(done){
+        if (done) {
             classNames += ' done';
         }
-        if(important){
+        if (important) {
             classNames += ' important'
         }
-    
+
         return (
         <span className={ classNames }>
            <span
             className="todo-list-item-label"
-            onClick={this.onLabelClick}
+            onClick={onToggleDone}
             >
                 {label}
             </span>
@@ -48,13 +37,15 @@ export default class TodoListItem extends Component{
             <button type="button" 
                 className="btn btn-outline-success 
                 btn-sm float-right"
-                    onClick={this.onMarkImportant}>
+                    onClick={onToggleImportant}>
                     
                     <i className="fa fa-exclamation"/>
             </button>
             <button type="button" 
                 className="btn btn-outline-danger 
-                btn-sm float-right">
+                btn-sm float-right"
+                onClick={onDeleted}
+                >
                     <i className="fa fa-trash-o"/>
             </button>
         </span>
